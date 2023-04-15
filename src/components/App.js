b47e5f4a2c5cd0a0ad7236d8bafc5a235069cb7a
Clifton Gockul <clifga@gmail.com>
2023-04-15 19:26:03 -0400

    completed lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,23 @@
-// create your App component here
+import React from "react";
+import { useState, useEffect } from "react";
+
+function App() {
+  const [renderDog, setRenderDog] = useState(null);
+
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((res) => res.json())
+      .then((data) => setRenderDog(data.message));
+  }, []);
+
+  if (!renderDog) {
+    return <p>Loading...</p>;
+  }
+  return (
+    <div>
+      <img src={renderDog} alt="A Random Dog"></img>
+    </div>
+  );
+}
+
+export default App;
